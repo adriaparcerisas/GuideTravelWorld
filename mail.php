20fa19017d901c19c29dbcd217566a480bc2a8b0
Adrià Parcerisas <adria.parcerisas.albes@gmail.com>
2020-04-25 02:25:28 +0200

    version 1.2

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+
+$message = $_POST['message'];
+$formcontent=" From: $name \n Subject: $subject \n Message: $message";
+$recipient = "info.guidetravelworld@gmail.com";
+/*$subject = "Contact Form";*/
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
